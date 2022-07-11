33c0acc064c3dccb1696628f4d54b71f9cc1b9fa
Shangze <89918650+DozeDuck@users.noreply.github.com>
2022-07-11 02:42:52 +0100

    Add files via upload

--- gro_merge.sh
+++ gro_merge.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
-# Usage: ./gro_merge.sh rec.gro lig_GMX.gro
+# Usage: ./gro_merge.sh rec.gro lig_GMX.gro lig_GMX.itp
 
 ###
 ### gro_merge.sh - merge rec.gro and lig_GMX.gro, also edit topol.top as well
 ###
 ### Usage:
-###       gro_merge.sh <receptor.gro>  <ligand_GMX.gro>
+###       gro_merge.sh <receptor.gro>  <ligand_GMX.gro>  <ligand_GMX.itp>
 ###
 ### Options:
-###     <receptor.gro> 		The gro file generated from gmx pdb2gmx
-###     <ligand_GMX.gro> 	The gro file generated from acpype
-###     -h          		Show this message
+###     <receptor.gro>		The gro file generated from gmx pdb2gmx
+###     <ligand_GMX.gro>		The gro file generated from acpype
+###     <ligand_GMX.itp>		The itp file generated fro acpype
+###     -h				Show this message
 
 help() {
 	sed -rn 's/^### ?//;T;p;' "$0"
@@ -27,6 +28,7 @@ more  $2 | sed -n '3,$p' > test.gro && sed -i '' test.gro
 a=`sed -n 2p $1`
 b=`sed -n 2p $2 `
 c=$(($a+$b))
+# echo $c
 more $1 > complex.gro
 sed -i "2c $c" complex.gro
 tail -1 $1 > last
@@ -37,15 +39,21 @@ rm last
 rm test.gro
 
 # Below is the part for editing topol.top
-echo "; Include ligand topology
-#include \"lig_GMX.itp\"
-#ifdef POSRES_LIG
-#include \"posre_lig.itp\"
-#endif
+echo "; Include ligand topology" >> include.dat
+# echo "#include \"lig_GMX.itp\"" >> include.dat
+echo "#include $3" >> include.dat
+echo "#ifdef POSRES_LIG" >> include.dat
+echo "#include \"posre_lig.itp\"" >> include.dat
+echo "#endif
 
-" >  include.dat
+" >>  include.dat
 
-sed -i '$a lig\ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ 1' topol.top 
+a=`more $3 | grep -A1 nrexcl | grep -v nrexcl`
+B=`echo $a | awk '{print $1}'`
+# sed -i '$a lig\ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ 1' topol.top 
+echo "lable"
+sed -i '$a lolololololololol' topol.top 
+sed -i "s/lolololololololol/$B              1/g" topol.top
 sed -i '22r include.dat'     topol.top
 rm include.dat
 
